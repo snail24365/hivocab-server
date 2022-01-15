4a02588abe89674cde505e56fa1d72f4fcad473a
Seunghui Choi <snail24365@gmail.com>
2022-01-16 04:22:58 +0900

    Feat: Viper를 사용하여 설정 변수들 관리

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,19 +7,18 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
+	"github.com/snail24365/hivocab-server/util"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/hivocab?sslmode=disable"
-)
-
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db :", err)
 	}
